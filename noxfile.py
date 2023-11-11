4e10790433d2df97e35a598fdb87d9ae4e5191c8
John Franey <1728528+johnfraney@users.noreply.github.com>
2023-11-11 12:16:38 -0400

    chore: remove flake8 from CI

--- noxfile.py
+++ noxfile.py
@@ -8,12 +8,6 @@ def tests(session):
 
 
 @nox.session(python=["3.11", "3.10"])
-def lint(session):
-    session.install("flake8", ".")
-    session.run("flake8", "blurry", "tests")
-
-
-@nox.session(python=["3.11", "3.10"])
 def typecheck(session):
     session.install("pyright", ".")
     session.run("pyright")
