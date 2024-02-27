de4d59b772b6ccfbe9e0dc6f91da8958ccee0841
John Franey <1728528+johnfraney@users.noreply.github.com>
2024-02-27 08:55:20 -0400

    feat: add support for Jinja filter plugins
    Adds support for plugins that add custom Jinja filters

--- blurry/plugins/__init__.py
+++ blurry/plugins/__init__.py
@@ -2,3 +2,4 @@ from importlib.metadata import entry_points
 
 discovered_markdown_plugins = entry_points(group="blurry.markdown_plugins")
 discovered_html_plugins = entry_points(group="blurry.html_plugins")
+discovered_jinja_filter_plugins = entry_points(group="blurry.jinja_filter_plugins")
