0f35b6d007a65054eb0dfd2348ee96f48a6deca1
John Franey <1728528+johnfraney@users.noreply.github.com>
2024-12-06 12:51:51 -0400

    feat: add JinjaX support
    JinjaX provides clean and composable components for tidier template authoring:
    
    https://jinjax.scaletti.dev/

--- blurry/templates.py
+++ blurry/templates.py
@@ -1,15 +1,21 @@
+import jinjax
 from jinja2 import Environment
 from jinja2 import FileSystemLoader
 from jinja2 import select_autoescape
+
 from blurry.plugins import discovered_jinja_extensions
 from blurry.plugins import discovered_jinja_filter_plugins
 from blurry.settings import get_templates_directory
 from blurry.settings import SETTINGS
 
 
-def get_jinja_env():
+def get_jinja_env() -> Environment:
+    """
+    Returns a Jinja environment complete with JinjaX and any installed Jinja extensions and filters.
+    """
+    templates_directory = get_templates_directory()
     jinja_env = Environment(
-        loader=FileSystemLoader(get_templates_directory()),
+        loader=FileSystemLoader(templates_directory),
         autoescape=select_autoescape(
             list(
                 {
@@ -31,4 +37,9 @@ def get_jinja_env():
                 f"Could not load Jinja filter plugin: {filter_plugin.name}. "
                 "Possibly because {filter_plugin.value} is not a valid object reference."
             )
+
+    jinja_env.add_extension(jinjax.JinjaX)
+    catalog = jinjax.Catalog(jinja_env=jinja_env)
+    catalog.add_folder(templates_directory / "components")
+
     return jinja_env
