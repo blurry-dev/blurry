085f42f0745c99ba6b8fb092c5ee96adcc5f7ab8
John Franey <1728528+johnfraney@users.noreply.github.com>
2025-11-18 14:25:23 -0400

    fix: Open Graph title missing for Thing schemas (#217)
    Fixes #216

--- tests/test_open_graph.py
+++ tests/test_open_graph.py
@@ -17,13 +17,38 @@ def test_open_graph_type_from_schema_type(schema_type, open_graph_type):
     assert open_graph_type_from_schema_type(schema_type) == open_graph_type
 
 
-schema_data = {"@type": "BlogPosting", "url": "/blog/article-one/", "image": "/hey.png"}
-meta_tag_content = """
+product_schema_data = {
+    "@type": "BlogPosting",
+    "headline": "My Cool Post",
+    "url": "/blog/article-one/",
+    "image": "/hey.png",
+}
+blog_posting_meta_tag_content = """
 <meta property="og:type" content="article">
+<meta property="og:title" content="My Cool Post">
 <meta property="og:url" content="/blog/article-one/">
 <meta property="og:image" content="/hey.png">
 """.strip()
 
+product_schema_data = {
+    "@type": "Product",
+    "name": "My Product",
+    "url": "/product/",
+    "image": "/product.png",
+}
+blog_posting_meta_tag_content = """
+<meta property="og:type" content="website">
+<meta property="og:title" content="My Product">
+<meta property="og:url" content="/product/">
+<meta property="og:image" content="/product.png">
+""".strip()
 
-def test_open_graph_meta_tags():
-    assert open_graph_meta_tags(schema_data) == meta_tag_content
+
+@pytest.mark.parametrize(
+    "schema_data, tag_content",
+    [
+        (product_schema_data, blog_posting_meta_tag_content),
+    ],
+)
+def test_open_graph_meta_tags(schema_data, tag_content):
+    assert open_graph_meta_tags(schema_data) == tag_content
