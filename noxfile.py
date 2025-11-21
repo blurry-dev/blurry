a17122bc0b281b51caa3c98c718d87e259a8b35b
John Franey <1728528+johnfraney@users.noreply.github.com>
2025-11-21 15:03:15 -0400

    chore: add support for Python 3.14 (#221)
    Updates CI to check for Python 3.14 so its support is more official

--- noxfile.py
+++ noxfile.py
@@ -1,13 +1,13 @@
 import nox
 
 
-@nox.session(python=["3.13", "3.12", "3.11"])
+@nox.session(python=["3.14", "3.13", "3.12", "3.11"])
 def tests(session):
     session.install("pytest", ".")
     session.run("pytest")
 
 
-@nox.session(python=["3.13", "3.12", "3.11"])
+@nox.session(python=["3.14", "3.13", "3.12", "3.11"])
 def typecheck(session):
     session.install("pyright", ".")
     session.run("pyright")
